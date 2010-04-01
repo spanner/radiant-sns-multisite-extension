c2421d825524d7e1ef180d83b541bf8c49d4e56b
spanner <will@spanner.org>
2010-04-01 11:24:37 +0100

    removed menu fiddling

--- sns_multisite_extension.rb
+++ sns_multisite_extension.rb
@@ -1,20 +1,15 @@
 class SnsMultisiteExtension < Radiant::Extension
   version "0.1"
-  description "Scopes javascripts and stylesheets to sites. It requires the spanner fork of multi_site"
+  description "Scopes javascripts and stylesheets to sites. Requires the rather experimental sites extension."
   url "http//github.com/netzpirat/radiant-sns-multisite-extension"
   
   extension_config do |config|
     config.extension 'sns'
-    # config.extension 'sites'
-    config.extension 'submenu'
+    config.extension 'sites'
   end
   
   def activate
-    TextAsset.send :is_site_scoped
-    admin.tabs.remove "CSS"
-    admin.tabs.remove "JS"
-    admin.tabs['Pages'].add_link('stylesheets', '/admin/css', :visibility => [:admin, :developer])
-    admin.tabs['Pages'].add_link('javascripts', '/admin/js', :visibility => [:admin, :developer])
+    TextAsset.send :has_site
   end
   
 end
